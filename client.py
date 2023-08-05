bd4b4fbc1152115a06aaf8f997a023eeb9ebdffd
rishu <rishuprasad220204@gmail.com>
2023-08-05 22:20:19 +0530

    Client and Server

--- client.py
+++ client.py
@@ -1,31 +1,99 @@
 import socket
-
-HEADER=64
-PORT= 5000
-FORMAT="utf-8"
+import threading
+HEADER = 64
+PORT = 5000
+FORMAT = "utf-8"
 DISCONNECT_MESSAGE = "[USER DISCONNECTED]"
-SERVER="20.0.9.132"
-ADDR=(SERVER,PORT)
+SERVER = "20.0.10.136"
+ADDR = (SERVER, PORT)
 
-client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a socket for the client
 client.connect(ADDR)
 
 def send(msg):
-    message=msg.encode(FORMAT)  #encode the message from string to bit
-    msg_length=len(message)
-    send_length=str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER -len(send_length))  #Adding padding to make the message 64 bit
+    message = msg.encode(FORMAT)    #encode the message from string to bit
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))   #Adding padding to make the message 64 bit
     client.send(send_length)
     client.send(message)
     print(client.recv(5000).decode(FORMAT))
-    
 
-# send("HELLO")
+def send_username(user):
+    message = user.encode(FORMAT)   #encode the message from string to bit
+    user_length = len(message)
+    send_length = str(user_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))   #Adding padding to make the message 64 bit
+    client.send(send_length)
+    client.send(message)
+
+def send_disconnect():
+    send(DISCONNECT_MESSAGE)
+    client.close()
+
+user = input("ENTER YOUR USERNAME: ")
+send_username(user)
+
+# receiver = input("WHO DO YOU WANT TO CHAT WITH: ")
+# # print(f"Connected with {receiver}")
+# send(f"{receiver}:[REQUEST]{user} WANTS TO CHAT WITH YOU")
+# choice = input("Do you want to send messages? (yes/no): ")
+# def sendMessageLoop():
+#     while True:
+    
+#         # request_message = f"[REQUEST]{user} WANTS TO CHAT WITH YOU"
+#         # send(f"{receiver}:{request_message}")  # Sending a request message
+        
+#         say = input("ENTER YOUR MESSAGE: ")
+#         send(f"{receiver}:{say}")
+       
+#         if say.lower() == "bye":
+#             send_disconnect()
+#             break
+# if choice.lower()=="yes":
+#     while True:
+    
+#         # request_message = f"[REQUEST]{user} WANTS TO CHAT WITH YOU"
+#         # send(f"{receiver}:{request_message}")  # Sending a request message
+        
+#         say = input("ENTER YOUR MESSAGE: ")
+#         send(f"{receiver}:{say}")
+       
+#         if say.lower() == "bye":
+#             send_disconnect()
+#             break
+#         send(f"{receiver}:{user} says: {say}")
+#     send_disconnect()
+
+        
+# #     message_thread = threading.Thread(target=sendMessageLoop)  #Start the message loop in a separate thread
+# #     message_thread.start()
+# #     message_thread.join()   #it waits for the message loop thread to complete before proceeding to close the client connection
+
+# # else:
+    
+# #     print("Waiting for the other user to start the conversation...")    # Wait for the recipient to initiate the conversation
+# #     response = client.recv(5000).decode(FORMAT)
+# #     print(response) # Print the message from the other user
+# # send_disconnect()
+# client.close()
+role_choice = input("Do you want to act as the sender or receiver? (sender/receiver): ")
+if role_choice.lower() == "sender":
+    receiver = input("WHO DO YOU WANT TO SEND TO: ")
+    message = input("ENTER YOUR MESSAGE: ")
+    send(f"{receiver}:{message}")
+else:
+    print("Waiting for incoming messages...")
+
 while True:
-    say=input("ENTER YOUR MESSAGE: ")
-    send(say)
-    disconnect="bye"
-    if say==disconnect:
-        send(DISCONNECT_MESSAGE)
+    msg = client.recv(5000).decode(FORMAT)
+    print(msg)
+    if msg.startswith("[sender]"):
+        reply = input("Enter your reply/message: ")
+        send(f"{msg[8:]}:{reply}")
+
+    if msg == DISCONNECT_MESSAGE:
+        print("Disconnected.")
         break
 
+send_disconnect()
--- server.py
+++ server.py
@@ -1,31 +1,57 @@
-import socket 
+import socket
 import threading
 
-HEADER=64
-PORT= 5000
-FORMAT="utf-8"
-SERVER=socket.gethostbyname(socket.gethostname())  #GETS HOST IP FROM THE SYSTEM
-ADDR=(SERVER,PORT)
+HEADER = 64
+PORT = 5000
+FORMAT = "utf-8"
+SERVER = socket.gethostbyname(socket.gethostname()) # Getting the IP address
+ADDR = (SERVER, PORT)
 DISCONNECT_MESSAGE = "[USER DISCONNECTED]"
 
-server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)  #AF_INET defines the IPv4 address,SOCK_STREAM streams the data through the server
-server.bind(ADDR)  #Bind the system IP and Port to the new Socket
+# Creating a socket for the server
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #AF_INET defines the IPv4 address,SOCK_STREAM streams the data through the server
+server.bind(ADDR)   #Bind the system IP and Port to the new Socket
 
-def handle_client(conn,addr):
-    print(f"[NEW CONNECTION] {addr} connected")
+# Dictionary to store user information: username -(conn, addr)
+user_data = {}
+
+def handle_client(conn, addr):
+    user = None
+
+    # Receive the username from the client
+    user_length = conn.recv(HEADER).decode(FORMAT)  #recv() waits until usernameis received and HEADER fixs the length of username
+    if user_length:
+        user_length = int(user_length)
+        user = conn.recv(user_length).decode(FORMAT)
+        user_data[user] = (conn, addr)
+    print(f"[NEW CONNECTION] {addr} connected as {user}")
+    for other_user, _ in user_data.items():
+        if other_user != user:
+            other_conn, _ = user_data[other_user]
+            other_conn.send(f"[REQUEST]{user} wants to chat with you.".encode(FORMAT))
     connected = True
     while connected:
-        msg_length = conn.recv(HEADER).decode(FORMAT)    #recv() waits until message is recived and HEADER fixs the length of message
-        # decode() convert the byte message into string using utf-8
+        msg_length = conn.recv(HEADER).decode(FORMAT)   #recv() waits until message is received and HEADER fixs the length of message
         if msg_length:
             msg_length = int(msg_length)
             msg = conn.recv(msg_length).decode(FORMAT)
             if msg == DISCONNECT_MESSAGE:   # In case the user disconnect
                 connected = False
-            print(f"[{addr}] {msg}")    # Print whatever the message is
+            else:
+                parts = msg.split(":", 1)   #Splits the message from :
+                if len(parts) == 2:
+                    receiver = parts[0]
+                    message = parts[1]
+                    if receiver in user_data:
+                        receiver_conn, _ = user_data[receiver]
+                        receiver_conn.send(f"[{user}] {message}".encode(FORMAT))    # Print whatever the message is
+                    else:
+                        print(f"USER[{receiver}] NOT FOUND!!")
+                else:
+                    print(f"INVALID MESSAGE FORMAT: {msg}")
 
-            conn.send("[MESSAGE RECEIVED]".encode(FORMAT))
     conn.close()    # Closes the connection
+
 
 def start():
     server.listen() #Puts the server into listen mode i.e. waiting for the connection to happen
